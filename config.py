d132933ed80a6987d5ef8d4376c506694371a177
Daniel Haliva <danielhaliva@edu.aac.ac.il>
2026-07-30 12:50:55 +0300

    Remove redundant admin.py file to fix import danielllr

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ DB_NAME = "TjBotDB"
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
 
 # מנהלים
-ADMINS = [8526860681]
+ADMINS = ["8526860681"]
 ADMIN_ID = ADMINS[0] if isinstance(ADMINS, list) and ADMINS else ADMINS
 
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
